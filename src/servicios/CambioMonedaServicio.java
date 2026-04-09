086780318598c9d70970bd4e4c3b28fda56c05a1
unknown <frayosorio@gmail.com>
2026-04-09 13:19:04 -0500

    Se agregan funciones para calcular estadisticas de los cambios de una moneda basado en programación funcional

--- src/servicios/CambioMonedaServicio.java
+++ src/servicios/CambioMonedaServicio.java
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -48,6 +48,64 @@ public class CambioMonedaServicio {
     public static Map<LocalDate, Double> getDatosGrafica(List<CambioMoneda> cambios) {
         return cambios.stream()
                 .collect(Collectors.toMap(cambio -> cambio.getFecha(), cambio -> cambio.getValor()));
+    }
+
+    public static List<Double> getCambios(List<CambioMoneda> cambios) {
+        return cambios.stream()
+                .map(cambio -> cambio.getValor())
+                .collect(Collectors.toList());
+    }
+
+    public static double getPromedio(List<Double> datos) {
+        return datos.stream()
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static double getMaximo(List<Double> datos) {
+        return datos.stream()
+                .mapToDouble(Double::doubleValue)
+                .max()
+                .orElse(0.0);
+    }
+
+    public static double getMinimo(List<Double> datos) {
+        return datos.stream()
+                .mapToDouble(Double::doubleValue)
+                .min()
+                .orElse(0.0);
+    }
+
+    public static double getDesviacionEstandar(List<Double> datos) {
+        double promedio = getPromedio(datos);
+        return datos.isEmpty() ? 0.0
+                : datos.stream()
+                        .mapToDouble(dato -> Math.abs(promedio - dato))
+                        .average()
+                        .orElse(0.0);
+    }
+
+    public static double getMediana(List<Double> datos) {
+        if (datos.isEmpty()) {
+            return 0.0;
+        }
+        var datosOrdenados = datos.stream().sorted().collect(Collectors.toList());
+        var n = datosOrdenados.size();
+
+        return n % 2 == 0 ? (datosOrdenados.get(n / 2) + datosOrdenados.get(n / 2 - 1)) / 2
+                : datosOrdenados.get(n / 2);
+    }
+
+    public static double getModa(List<Double> datos) {
+        return datos.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(0.0);
+
     }
 
 }
